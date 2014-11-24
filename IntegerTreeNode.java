79e3849730990aa5dc5125ffbb7ac443cb0ea439
unknown <lpalme03@CSPC095.dcs.bbk.ac.uk>
2014-11-24 19:07:11 +0000

    First version of IntegerTreeNode code - simple ToString()

---
+++ IntegerTreeNode.java
@@ -0,0 +1,155 @@
+public class IntegerTreeNode{
+	private int value;
+	private IntegerTreeNode left;
+	private IntegerTreeNode right;
+
+	public IntegerTreeNode(int value)
+	{
+		this.value = value;
+		this.right = null;
+		this.left = null;
+	}
+
+	public boolean add(int value)
+	{
+		if(this.value > value)
+		{
+			if(this.left != null)
+			{
+				this.left.add(value);
+			}
+			else
+			{
+				this.left = new IntegerTreeNode(value);
+			}
+		}
+		else if(this.value < value)
+		{
+			if(this.right != null)
+			{
+				this.right.add(value);
+			}
+			else
+			{
+				this.right = new IntegerTreeNode(value);
+			}
+		}
+		else
+		{
+			return false;
+		}
+		return true;
+	}
+
+	public boolean contains(int value)
+	{
+		boolean result = false;
+		if(this.value == value)
+		{
+			return true;
+		}
+		else
+		{
+			if(this.value < value)
+			{
+				if(this.right != null)
+				{
+					result = this.right.contains(value);
+				}
+				else
+				{
+					result = false;
+				}
+			}
+			else
+			{
+				if(this.left != null)
+				{
+					result = this.left.contains(value);
+				}
+				else
+				{
+					result = false;
+				}
+			}
+		}
+		return result;
+	}
+
+	public int getMax()
+	{
+		int result = 0;
+		if(this.right != null)
+		{
+			result = this.right.getMax();
+		}
+		else
+		{
+			result = this.value;
+		}
+		return result;
+	}
+
+	public int getMin()
+	{
+		int result = 0;
+		if(this.left != null)
+		{
+			result = this.left.getMin();
+		}
+		else
+		{
+			result = this.value;
+		}
+		return result;
+	}
+
+	public static void main(String[] args)
+	{
+		IntegerTreeNode basenode = new IntegerTreeNode(6);
+		basenode.add(10);
+		basenode.add(15);
+		basenode.add(-1);
+		basenode.add(7);
+		basenode.add(3);
+		basenode.add(2);
+		if(basenode.contains(3))
+		{
+			System.out.println("This tree contains 3");
+		}
+		if(basenode.contains(7))
+		{
+			System.out.println("This tree contains 7");
+		}
+		if(basenode.contains(17))
+		{
+			System.out.println("This tree contains 17");
+		}
+		System.out.println("The mininmum is: " + basenode.getMin());
+		System.out.println("The maximum is: " + basenode.getMax());
+		System.out.println(basenode.ToString());
+	}
+
+	public String ToString()
+	{
+		String output = "";
+		output += this.value;
+		if(this.left != null)
+		{
+			output = output + "L[" + this.left.ToString() + "]";
+		}
+		else
+		{
+			output = output + "L[]";
+		}
+		if(this.right != null)
+		{
+			output = output + "R[" + this.right.ToString() + "]";
+		}
+		else
+		{
+			output = output + "R[]";
+		}
+		return output;
+	}
+}
