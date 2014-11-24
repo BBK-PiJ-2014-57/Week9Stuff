54fb0c4aa022e23d68785bee46289daf899d9526
unknown <lpalme03@CSPC095.dcs.bbk.ac.uk>
2014-11-24 20:50:35 +0000

    New class with not working remove function

--- IntegerTreeNode.java
+++ IntegerTreeNode.java
@@ -113,6 +113,10 @@ public class IntegerTreeNode{
 		basenode.add(7);
 		basenode.add(3);
 		basenode.add(2);
+		basenode.add(-3);
+		basenode.add(4);
+		basenode.add(-5);
+		basenode.add(-6);
 		if(basenode.contains(3))
 		{
 			System.out.println("This tree contains 3");
@@ -127,6 +131,10 @@ public class IntegerTreeNode{
 		}
 		System.out.println("The mininmum is: " + basenode.getMin());
 		System.out.println("The maximum is: " + basenode.getMax());
+		System.out.println(basenode.ToString());
+		System.out.println("The depth of this tree is: " + basenode.depth());
+		basenode = basenode.remove(3);
+		System.out.println("Removed 3!");
 		System.out.println(basenode.ToString());
 	}
 
@@ -146,4 +154,75 @@ public class IntegerTreeNode{
 
 		return output;
 	}
+
+	public int depth()
+	{
+		int length = 0;
+		int temp = 0;
+		if(this.left != null)
+		{
+			temp = this.left.depth()+1;
+			if(temp > length)
+			{
+				length = temp;
+			}
+		}
+		if(this.right != null)
+		{
+			temp = this.right.depth()+1;
+			if(temp > length)
+			{
+				length = temp;
+			}
+		}
+		return length;
+	}
+
+	public IntegerTreeNode remove(int value)
+	{
+		IntegerTreeNode = null;
+		if(this.value != value)
+		{
+			if(this.value < value)
+			{
+				this.right = this.right.remove(value);
+			}
+			else if(this.value > value)
+			{
+				this.left = this.left.remove(value);
+			}
+		}
+		else
+		{
+			if(this.right != null)
+			{
+				int min = this.right.getMin();
+				IntegerTreeNode upmin = this.right.NodeWith(min);
+				upmin.left = this.left;
+				toreturn = this.right;
+			}
+			else
+			{
+				toreturn = this.left;
+			}
+		}
+		return toreturn;
+	}
+
+	public IntegerTreeNode NodeWith(int value)
+	{
+		if(this.value == value)
+		{
+			return this;
+		}
+		else if(this.value > value)
+		{
+			return this.left.NodeWith(value);
+		}
+		else
+		{
+			return this.right.NodeWith(value);
+		}
+	}
+
 }
